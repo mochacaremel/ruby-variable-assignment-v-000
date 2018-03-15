6e26580127500f030bccba8f8f0f0e585d4d78f5
Asheley <asheleymcdonald@gmail.com>
2018-03-15 00:32:54 +0000

    automatically backed up by learn

--- variable.rb
+++ variable.rb
@@ -1,7 +1,7 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
 # Code your solution in variable.rb
-describe "./variable.rb" do
+puts "./variable.rb" do
 
   it "defined a local variable called greeting and set it equal to 'Hello World'" do
     greeting = get_variable_from_file('./variable.rb', "greeting")
